107f6400def723d15fdebe305cade0647c84beaa
sethlangel <seth@blueplate.ai>
2024-10-06 18:05:06 -0700

    Version 2 - Implements /admin/reset API

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,5 +16,11 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("""UPDATE store_info SET gold = 100"""))
+        connection.execute(sqlalchemy.text("""UPDATE potion_inventory SET quantity = 0"""))
+        connection.execute(sqlalchemy.text("""UPDATE liquid_quantity SET quantity = 0"""))
+
     return "OK"
 
